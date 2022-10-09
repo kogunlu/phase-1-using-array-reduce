104dbad3d4eb4871e1d40342a6c2adc2c09a32f4
Kuday Ogunlu <kudayogunlu@gmail.com>
2022-10-09 16:57:16 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+function addValues(curValue,preValue){
+    return curValue + preValue
+}
+
+const totalBatteries = batteryBatches.reduce(addValues,0)
